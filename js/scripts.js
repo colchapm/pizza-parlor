a0b686694e8c9dbfca3f21091dd0f626d849e17e
Collin Chapman <cchap14@gmail.com>
2021-02-26 20:46:03 -0800

    create test number 1, creating a new pizza object

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+//Business Logic
+
+function Pizza(size, meat, veggies) {
+  this.size = size;
+  this.meat = meat;
+  this.veggies = veggies;
+  this.currentPrice = 0;
+}
+
+
+
+
+
+
+
+//User Interface Logic
+
+
+let newOrder = new Pizza("10 inch", "pepperoni", "mushrooms")
