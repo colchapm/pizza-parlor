aacf1b93c036fb98b1c112e38fcb0361e808c4b4
Collin Chapman <cchap14@gmail.com>
2021-02-26 20:51:45 -0800

    if size equals 10inch return 14

--- js/scripts.js
+++ js/scripts.js
@@ -7,8 +7,18 @@ function Pizza(size, meat, veggies) {
   this.currentPrice = 0;
 }
 
-
-
+Pizza.prototype.sizePrice = function(size) {
+  if (this.size === "6 inch") {
+    this.currentPrice = 6;
+  } 
+  else if (this.size === "10 inch") {
+    this.currentPrice = 14;
+  }
+  else {
+    this.currentPrice = 18;
+  }
+  return this.currentPrice;
+}
 
 
 
